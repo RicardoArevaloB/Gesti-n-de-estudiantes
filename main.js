a0c77ab5d9a95fcc062d804d7e595ee97f88b099
RicardoArevaloB <160280340+RicardoArevaloB@users.noreply.github.com>
2024-09-05 12:12:12 -0500

    feat: :sparkles: creacion de los archivos y funciones
    se crearon los archivos html, css y js tambien se agregaron funciones y comunicacion entre los archivos

---
+++ main.js
@@ -0,0 +1,11 @@
+const miform = document.querySelector('#miform');
+//funcion agregar estuduante 
+
+let estudaintes = [];
+
+
+miform.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const nombre = document 
+})
